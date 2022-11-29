e16fb93e06512efb28a7f2fda278573b4093d379
Umong51 <umwng9450@gmail.com>
2022-11-29 13:40:33 +0600

    Modified cmp and lspconfig

--- lua/umong51/lspconfig.lua
+++ lua/umong51/lspconfig.lua
@@ -1,5 +1,7 @@
-local status, nvim_lspconfig = pcall(require, "lspconfig")
-if (not status) then return end
+local lspconfig_status, lspconfig = pcall(require, "lspconfig")
+if (not lspconfig_status) then return end
+local cmp_nvim_lsp_status, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+if (not cmp_nvim_lsp_status) then return end
 
 local on_attach = function(client, bufnr)
   -- format on save
@@ -12,27 +14,29 @@ local on_attach = function(client, bufnr)
   end
 end
 
-nvim_lspconfig.sumneko_lua.setup {
+local capabilities = cmp_nvim_lsp.default_capabilities()
+
+lspconfig["sumneko_lua"].setup({
+  capabilities = capabilities,
   on_attach = on_attach,
-  settings = {
+  settings = { -- custom settings for lua
     Lua = {
-      runtime = {
-        version = 'LuaJIT',
-      },
+      -- make the language server recognize "vim" global
       diagnostics = {
-        globals = {'vim'},
+        globals = { "vim" },
       },
       workspace = {
+        -- make language server aware of runtime files
         library = {
           [vim.fn.expand("$VIMRUNTIME/lua")] = true,
           [vim.fn.stdpath("config") .. "/lua"] = true,
         },
       },
-      telemetry = {
-        enable = false,
-      },
     },
   },
-}
+})
 
-nvim_lspconfig.pyright.setup{ on_attach = on_attach }
+lspconfig.pyright.setup {
+  capabilities = capabilities,
+  on_attach = on_attach
+}
--- lua/umong51/mason.lua
+++ lua/umong51/mason.lua
@@ -1,10 +1,10 @@
-local status, mason = pcall(require, "mason")
-if (not status) then return end
-local status2, lspconfig = pcall(require, "mason-lspconfig")
-if (not status2) then return end
+local mason_status, mason = pcall(require, "mason")
+if (not mason_status) then return end
+local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
+if (not mason_lspconfig_status) then return end
 
 mason.setup()
-lspconfig.setup({
+mason_lspconfig.setup({
   ensure_installed = { "sumneko_lua", "pyright" }
 })
 
