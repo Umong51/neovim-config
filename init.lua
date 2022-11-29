0fafea680ff15937440db5cc81358e64dd008e85
Umong51 <umwng9450@gmail.com>
2022-11-30 01:30:55 +0600

    Moved lsp related files to lsp/ subdir

--- init.lua
+++ init.lua
@@ -1,15 +1,15 @@
-require("umong51/plugins")
-require("umong51/bufferline")
-require("umong51/cmp")
-require("umong51/comment")
-require("umong51/gitsigns")
-require("umong51/lspconfig")
-require("umong51/lspsaga")
-require("umong51/lualine")
-require("umong51/mason")
-require("umong51/nvim-tree")
-require("umong51/nvim-treesitter")
-require("umong51/telescope")
+require("umong51.plugins")
+require("umong51.bufferline")
+require("umong51.cmp")
+require("umong51.comment")
+require("umong51.gitsigns")
+require("umong51.lsp.lspconfig")
+require("umong51.lsp.lspsaga")
+require("umong51.lsp.mason")
+require("umong51.lualine")
+require("umong51.nvim-tree")
+require("umong51.nvim-treesitter")
+require("umong51.telescope")
 
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -17,7 +17,7 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.mouse = nil
-vim.cmd([[au Filetype python setl et ts=4 sw=4]])
+vim.cmd([[autocmd Filetype python setlocal expandtab tabstop=4 shiftwidth=4]])
 vim.keymap.set('n', '<esc>', ':noh<cr><esc>', { silent = true })
 
 -- Colorscheme
